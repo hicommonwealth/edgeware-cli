735a104accc7696670b00b23b9cf36698fdacfb7
Drew Stone <drewstone329@gmail.com>
2019-01-02 19:28:45 +0200

    Update lint and index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import { ApiPromise } from '@polkadot/api';
 import { WsProvider } from '@polkadot/rpc-provider';
+import { IdentityTypes } from './identity';
+import { GovernanceTypes } from './governance';
 
 export default async function (remoteNodeUrl) {
-    const options = {
-      provider : new WsProvider('ws://' + remoteNodeUrl),
-      types : {
-        ...IdentityTypes,
-        ...GovernanceTypes,
-      },
-    };
+  const options = {
+    provider : new WsProvider('ws://' + remoteNodeUrl),
+    types : {
+      ...IdentityTypes,
+      ...GovernanceTypes,
+    },
+  };
 
-    return await ApiPromise.create(options);
+  return await ApiPromise.create(options);
 }
