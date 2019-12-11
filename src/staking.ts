f074e9065c268b13cb6300957c43b2717c01700f
Drew Stone <drewstone329@gmail.com>
2019-12-11 21:41:21 +0200

    Add errors for super large staking inputs

--- src/staking.ts
+++ src/staking.ts
@@ -14,14 +14,17 @@ export default function (mod, func, cArgs) {
   }
 
   if (mod === 'staking' && func === 'unbond') {
+    if (Number(cArgs[0]) >= 10 ** 9) throw new Error('You are trying to send more than 1,000,000,000 EDG, staking units are in units of EDG')
     cArgs[0] = String((10 ** 18) * Number(cArgs[0]));
   }
 
   if (mod === 'staking' && func === 'bond') {
+    if (Number(cArgs[1]) >= 10 ** 9) throw new Error('You are trying to send more than 1,000,000,000 EDG, staking units are in units of EDG')
     cArgs[1] = String((10 ** 18) * Number(cArgs[1]));
   }
 
   if (mod === 'staking' && func === 'bondExtra') {
+    if (Number(cArgs[0]) >= 10 ** 9) throw new Error('You are trying to send more than 1,000,000,000 EDG, staking units are in units of EDG')
     cArgs[0] = String((10 ** 18) * Number(cArgs[0]));
   }
 
